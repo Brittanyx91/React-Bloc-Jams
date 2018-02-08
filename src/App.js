1a378a989b6c4949f502e77f6aac74f42259f1a8
Brittany Blakley <brittany.m.blakley@gmail.com>
2018-02-08 18:03:53 -0500

    Fixed Album Page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
- import './App.css';
- import Landing from './components/Landing';
-  import Library from './components/Library';
+import './App.css';
+import Landing from './components/Landing';
+import Library from './components/Library';
+import Album from './components/Album';
 
  class App extends Component {
    render() {
@@ -17,7 +18,8 @@ import { Route, Link } from 'react-router-dom';
          </header>
          <main>
          <Route exact path="/" component={Landing} />
-           <Route path="/library" component={Library} />
+         <Route path="/library" component={Library} />
+         <Route path="/album/:slug" component={Album} />
          </main>
        </div>
      );
